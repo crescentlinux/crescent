7401bc9a10b5519a31de998c0810c689d0e014ee
satomi <92891760+satomiify@users.noreply.github.com>
2024-08-19 16:14:19 -0400

    Update install_crescent.sh

--- install_crescent.sh
+++ install_crescent.sh
@@ -11,7 +11,7 @@ export PATH="/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export REPO_BASE="http://www.cres.rf.gd"
 export VERSION_FLAG=https://cdn.asahilinux.org/installer/latest
 export INSTALLER_BASE=https://cdn.asahilinux.org/installer
-export INSTALLER_DATA="$REPO_BASE"/deepin-m1/installer_data.json
+export INSTALLER_DATA="$REPO_BASE"/installation/installer_data.json
 
 #TMP="$(mktemp -d)"
 TMP=/tmp/asahi-install
