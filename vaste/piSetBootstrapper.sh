18dd66d40df46a753e70b627ab8bbde21efc9aea
vastra <92891760+qvast@users.noreply.github.com>
2024-09-13 23:19:01 -0400

    add asahi bootstrapper ref // real initial commit

--- vaste/piSetBootstrapper.sh
+++ vaste/piSetBootstrapper.sh
@@ -164,6 +164,6 @@ EOF
 done
    done
 
-   echo " [>] Completed setup!"
+   echo " [>] Completed setup."
    echo " [>] Starting Asahi Linux Installer.."
    curl -L --no-progress-meter https://raw.githubusercontent.com/crescentlinux/crescent/main/apple-m1-asahi/crescm1.txt | sh
